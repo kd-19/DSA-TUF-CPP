131450776b4d7bb271a34ebda94c9a34b87bebea
Kuldeep Sharma <sharmakd2736@gmail.com>
2025-10-15 00:24:46 +0530

    Pattern 8 Solution

---
+++ Patterns/Pattern 8/index.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+        void pattern8(int n){
+            for(int i=n; i>=1; i--){
+
+                for(int j=0; j<n-i; j++){
+                    cout<<" ";
+                }
+                for(int j=1; j<= 2*i - 1; j++){
+                    cout<<"*";
+                }
+                for(int j=0; j<n-i; j++){
+                    cout<<" ";
+                }
+
+                cout<<endl;
+            }
+        }
+};
+
+int main(){
+    int N=5;
+    Solution sol;
+    sol.pattern8(N);
+    return 0;
+}

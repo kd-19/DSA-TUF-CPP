c2b9e6e15068eced2c5c5151cc01c9e466825135
Kuldeep Sharma <sharmakd2736@gmail.com>
2025-10-19 15:06:45 +0530

    GCD of two numbers

---
+++ Basic Math/GCD of two numbers/index.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int GCD(int n1, int n2){
+    while(n1 > 0 && n2 > 0){
+        if(n1 > n2){
+            n1 = n1%n2;
+        }else{
+            n2 = n2%n1;
+        }
+    }
+    if(n1 == 0) return n2;
+
+    return n1;
+}
+
+int main(){
+    int n1=6, n2=8;
+    cout<<GCD(n1, n2);
+    return 0;
+}
